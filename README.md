0fb9fd477794122c899f5edbb5ef78e133c99fbb
Claudio Verdú Ruiz <claudio.verdu@babel-si.local>
2018-10-25 14:36:38 +0200

    Update README

--- README.md
+++ README.md
@@ -2,12 +2,29 @@
 
 ## Instructions
 
+### Environment
+
+````
 git clone https://github.com/claverru/generatext.git
-
 cd generatext
-
 docker build -t generatext .
+````
 
-### For development
+### Download Wikipedia data with WikiExtractor
+````
+# apt-get install git
+git clone https://github.com/attardi/wikiextractor.git
+cd wikiextractor
+python setup.py install
+cd .. 
+# apt-get install wget
+# eswiki, enwiki, itwiki, etc 
+wget http://download.wikimedia.org/eswiki/latest/eswiki-latest-pages-articles.xml.bz2
+WikiExtractor.py -cb 250K -o extracted itwiki-latest-pages-articles.xml.bz2
+````
 
+### Development
+
+````
 docker run -it -v "%CD%":/usr/src generatext bash
+````
--- text_mining.py
+++ text_mining.py
@@ -17,6 +17,10 @@ with bz2.open('extracted/AA/wiki_01.bz2', 'rt') as inputfile:
 		line = inputfile.readline()
 """
 
+folders = 0
+files = 0
+lines = 0
+
 print('Procesing text')
 for subdirectory_name in os.listdir('extracted'):
 	for file_name in os.listdir('extracted/{}'.format(subdirectory_name)):
@@ -24,7 +28,12 @@ for subdirectory_name in os.listdir('extracted'):
 			line = inputfile.readline()
 			while line:
 				docs.append(nlp(line))
+				lines += 1
+				print('Folders: {}, Files: {}, Lines: {}'.format(folders, files, lines), end='\r')
 				line = inputfile.readline()
+		files += 1
+	folders += 1
+
 
 print('Selecting sentences')
 docs = [doc for doc in docs if len(doc) > 10 and len(doc) < 40 and doc[0].text[0].isupper()]
